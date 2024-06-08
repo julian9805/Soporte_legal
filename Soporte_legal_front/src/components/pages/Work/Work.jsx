590ba05fdbacc910cda817bb9d9885b0c448d1c3
julian9805 <juliancamilolopezcabrera@gmail.com>
2024-06-08 03:13:47 +0200

    cambios work

--- Soporte_legal_front/src/components/pages/Work/Work.jsx
+++ Soporte_legal_front/src/components/pages/Work/Work.jsx
@@ -1,23 +1,33 @@
-import Footer from "../Footer";
-import Header from "../Header";
+
 import "./Work.scss";
 
 
 function Work(){
 
+    
+
     return(
         <> 
             <section className="work-container">
                 <div className="work-title">
-                    <h3>RAMAS DEL DERECHO:</h3>
+                    <h2>RAMAS DEL DERECHO:</h2>
                 </div>
                 <div className="work-list">
-                    <p>DERECHO CIVIL:</p>
-                    <span>Temas legales entre personas naturales y/o jurídicas (pertenencias, divisorios, posesorios, propiedad horizontal, responsabilidad civil)</span>
-                    <p>PENAL:</p>
-                    <span>Nuestros abogados penalistas le representarán y acompañarán en cada etapa del proceso penal. (derecho penitenciario, defensa penal, delitos financieros, etc.)</span>
-                    <p>DERECHO LABORAL:</p>
-                    <span>Temas legales que involucren empleados y empleadores (Indemnizaciones, despidos, contratación, sindicatos, etc.)</span>
+                    <div className="item" id="civil">
+                        <h3>DERECHO CIVIL:</h3>
+                        <p>Temas legales entre personas naturales y/o jurídicas (pertenencias, divisorios, posesorios, propiedad horizontal, responsabilidad civil)</p>
+                    </div>
+                    
+                    <div className="item" id="penal">
+                        <h3>PENAL:</h3>
+                        <p>Nuestros abogados penalistas le representarán y acompañarán en cada etapa del proceso penal. (derecho penitenciario, defensa penal, delitos financieros, etc.)</p>
+                    </div>
+                    
+                    <div className="item" id="laboral">
+                        <h3>DERECHO LABORAL:</h3>
+                        <p>Temas legales que involucren empleados y empleadores (Indemnizaciones, despidos, contratación, sindicatos, etc.)</p>
+                    </div>
+                    
                 </div>
             </section>
         </>
